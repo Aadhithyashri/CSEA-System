84400893d9d3cc455a8a0b77567966ceaa535841
Aadhithyashri Manikandan <aadhithyashri@gmail.com>
2022-05-30 01:12:43 +0530

    prefinal

--- src/components/Content.js
+++ src/components/Content.js
@@ -51,7 +51,8 @@ export default function Content() {
                 </div>
               </div>
             </div>
-
+            <br></br>
+            <br></br>
         </div>
     </div>
   )
--- src/components/ContentProposal.js
+++ src/components/ContentProposal.js
@@ -5,7 +5,109 @@ export default function ContentProposal() {
   return (
     <div className="OutBg">
         <div className="CenterBox">
+            <p className="EventPHeading">Event Proposal</p>
+            <div className='Year'>
+              <p>Year</p>
+              <div className="YearBox">
+              </div>
+            </div>
+
+            <div className="EventName">
+              <p>Event Name</p>
+              <div className="EventNBox">
+              </div>
+            </div>
+
+            <div className="FromDate">
+              <p>From Date</p>
+              <div className="FromDBox">
+              </div>
+            </div>
+
+            <div className="ToDate">
+              <p>To Date</p>
+              <div className="ToDBox">
+              </div>
+            </div>
+
+            <div className="Venue">
+              <p>Venue</p>
+              <div className="VenueBox">
+              </div>
+            </div>
+
+            <div className="FromTime">
+              <p>From Time</p>
+              <div className="FromTBox">
+              </div>
+            </div>
+
+            <div className="ToTime">
+              <p>To Time</p>
+              <div className="ToTimeBox">
+              </div>
+            </div>
             
+            <div className="ExpPC">
+              <p>Expected Participant Count</p>
+              <div className="ExpPCBox">
+              </div>
+            </div>
+
+            <div className="ChiefGuest">
+              <p>Chief Guest</p>
+              <div className="ChiefGBox">
+              </div>
+            </div>
+
+            <div className="Designation">
+              <p>Designation</p>
+              <div className="DesigBox">
+              </div>
+            </div>
+
+            <div className="Address">
+              <p>Address</p>
+              <div className="AddressBox">
+              </div>
+            </div>
+
+            <div className="President">
+              <p>President</p>
+              <div className="PresidentBox">
+              </div>
+            </div>
+
+            <div className="ExpE">
+              <p>Expected Expense</p>
+              <div className="ExpEBox">
+              </div>
+            </div>
+
+            <div className="TotAmtA">
+              <p>Total Amount Allocated by SU</p>
+              <div className="TotAmtABox">
+              </div>
+            </div>
+
+            <div className="TotAmtS">
+              <p>Total amount spent on the day of request</p>
+              <div className="TotAmtSBox">
+              </div>
+            </div>
+
+            <div className="Comments">
+              <p>Comments</p>
+              <div className="CommentsBox">
+              </div>
+            </div>
+
+            <div className="EventDesc">
+              <p>Event Description</p>
+              <div className="EventDescBox">
+              </div>
+            </div>
+
         </div>
     </div>
   )
